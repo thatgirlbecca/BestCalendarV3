11238162ded202747a0d3e594afba1bcf342728c
thatgirlbecca <becca@oakleyfam.com>
2025-12-10 12:36:17 -0500

    Add config.js for deployment

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const SUPABASE_CONFIG = {
+  url: 'https://hiqlcsxgznbeedyorugn.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpcWxjc3hnem5iZWVkeW9ydWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MTE2MzcsImV4cCI6MjA4MDI4NzYzN30.XKNS0G-ozv8u1o2ZhZ6UMy51cdm8vES6oZcfQKovQDs'
+};
